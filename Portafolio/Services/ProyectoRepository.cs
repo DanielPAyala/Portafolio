431755c08d97dd47a71e3d6f13c6d588386e44c4
Carlos Daniel <carlosporrasayala@gmail.com>
2024-03-11 22:36:15 -0500

    feat | Agregar proyectos desplegados y sus repo

--- Portafolio/Services/ProyectoRepository.cs
+++ Portafolio/Services/ProyectoRepository.cs
@@ -13,33 +13,44 @@ namespace Portafolio.Services
         {
             return new List<ProyectoDTO>
             {
-                new ProyectoDTO
-                {
-                    Titulo = "Amazon",
-                    Descripcion = "E-Commerce realizado en ASP.NET Core",
-                    Link = "https://amazon.com",
-                    ImagenURL = "/img/amazon.png"
+                new ProyectoDTO {
+                    Titulo = "Pipes App",
+                    Descripcion = "Listado de pipes comunes y personalizados",
+                    Link = "https://danielpayala.github.io/pipes-app/",
+                    ImagenURL = "/img/projects/pipes-app.png",
+                    Repo = "https://github.com/DanielPAyala/pipes-app/tree/develop"
                 },
                 new ProyectoDTO
                 {
-                    Titulo = "New York Times",
-                    Descripcion = "Página de noticias en React",
-                    Link = "https://nytimes.com",
-                    ImagenURL = "/img/nyt.png"
-                },
-                new ProyectoDTO
-                {
-                    Titulo = "Reddit",
-                    Descripcion = "Red social para compartir en comunidades",
-                    Link = "https://reddit.com",
-                    ImagenURL = "/img/reddit.png"
-                },
-                new ProyectoDTO
-                {
-                    Titulo = "Steam",
-                    Descripcion = "Tienda en línea para comprar videojuegos",
+                    Titulo = "Portafolio",
+                    Descripcion = "Landing Page",
                     Link = "https://store.steampowered.com",
-                    ImagenURL = "/img/steam.png"
+                    ImagenURL = "/img/projects/portafolio.png",
+                    Repo = "https://github.com/DanielPAyala/Portafolio"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "Country App",
+                    Descripcion = "Single Page Application",
+                    Link = "https://danielpayala.github.io/country-app",
+                    ImagenURL = "/img/projects/countries-app.png",
+                    Repo = "https://github.com/DanielPAyala/country-app"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "Gifs Apps",
+                    Descripcion = "Buscador de gifs",
+                    Link = "https://danielpayala.github.io/gifs-app/",
+                    ImagenURL = "/img/projects/gifs-app.png",
+                    Repo = "https://github.com/DanielPAyala/gifs-app/tree/develop"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "DBZ Personajes",
+                    Descripcion = "Profundizar un poco más en los módulos, FormsModule, ngModel, @Inputs, @outputs, Servicios, Métodos en servicios",
+                    Link = "https://danielpayala.github.io/angular-bases-deploy/",
+                    ImagenURL = "/img/projects/dbz-personajes.png",
+                    Repo = "https://github.com/DanielPAyala/angular-bases-deploy"
                 }
             };
         }
